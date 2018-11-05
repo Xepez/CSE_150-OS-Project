0e97de44b9ddefef4c15dc276be283e375f093d4
Brennan Armstrong <BDABrennan@gmail.com>
2018-11-04 22:40:32 -0800

    Update Boat.java
    Cleaned up code, possibly introduced an error

--- threads/Boat.java
+++ threads/Boat.java
@@ -4,52 +4,42 @@ import nachos.threads.KThread;
 import nachos.threads.Condition2;
 import nachos.threads.Lock;
 import nachos.machine.Lib;
-//oahu ->Malokai
 
 import nachos.ag.BoatGrader;
 import nachos.machine.Lib;
-
 public class Boat {
+	//Global Variables
+	static int oAdult, oChild;
+	static boolean boatOnOahu;
+	static int inBoat;
+	static Lock genLock;
+	static Condition2 adultsOahu, adultsMolokai;
+	static Condition2 childrenOahu, childrenMolokai;
+	static Semaphore done;
 	static BoatGrader bg;
 
 	public static void selfTest() {
 		BoatGrader b = new BoatGrader();
-
-		// System.out.println("\n ***Testing Boats with only 2 children***");
-		// begin(0, 2, b);
-
-		// System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
-		// begin(1, 2, b);
-
 		System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
 		begin(3, 3, b);
-
-		// System.out.println("\n ***Testing Boats with 20 children, 10 adults***");
-		// begin(10, 20, b);
 	}
 
 	public static void begin(int adults, int children, BoatGrader b) {
 		Lib.assertTrue(children >= 2);
 		Lib.assertTrue(b != null);
-
-		// Store the externally generated autograder in a class
-		// variable to be accessible by children.
 		bg = b;
-
-		// Instantiate global variables here
-		oAdult = adults;
-		oChild = children;
-		inBoat = 0;
+		
+		//init global variables
+		oAdult = adults;	//Oahu adult count
+		oChild = children;	//Oahu children count
+		inBoat = 0;			//# people in boat
 		boatOnOahu = true;
-		commonLock = new Lock();
-		adultsOahu = new Condition2(commonLock);
-		adultsMolokai = new Condition2(commonLock);
-		childrenOahu = new Condition2(commonLock);
-		childrenMolokai = new Condition2(commonLock);
+		genLock = new Lock();
+		adultsOahu = new Condition2(genLock);
+		childrenOahu = new Condition2(genLock);
+		adultsMolokai = new Condition2(genLock);
+		childrenMolokai = new Condition2(genLock);
 		done = new Semaphore(0);
-
-		// Create threads here. See section 3.4 of the Nachos for Java
-		// Walkthrough linked from the projects page.
 		for (int i = 0; i < adults; i++) {
 			Runnable r = new Runnable() {
 				public void run() {
@@ -57,29 +47,29 @@ public class Boat {
 				}
 			};
 			KThread t = new KThread(r);
-			t.setName("Adult " + i + "Thread");
+			t.setName("Adult " + i + " Thread");
 			t.fork();
 		}
 
-		for (int i = 0; i < children; i++) {
+		for (int i = 0; i < oChild; i++) {
 			Runnable r = new Runnable() {
 				public void run() {
 					ChildItinerary();
 				}
 			};
 			KThread t = new KThread(r);
-			t.setName("Child " + i + "Thread");
+			t.setName("Child " + i + " Thread");
 			t.fork();
 		}
 
 		done.P();
 
-		System.out.println(" ***Boat Test ends!***");
+		System.out.println("Boat test over");
 	}
 
 	static void AdultItinerary() {
 		boolean onOahu = true;
-		commonLock.acquire();
+		genLock.acquire();
 		while (true) {
 			Lib.assertTrue(onOahu);
 			if (inBoat == 0 && boatOnOahu && oChild <= 1) {
@@ -101,9 +91,9 @@ public class Boat {
 
 	static void ChildItinerary() {
 		boolean onOahu = true;
-		commonLock.acquire();
-		while (true)
-			if (onOahu) {
+		genLock.acquire();
+		while(true)
+			if(onOahu) {//if Oahu
 				if (boatOnOahu && inBoat == 0) {
 					onOahu = false;
 					oChild--;
@@ -124,8 +114,8 @@ public class Boat {
 					childrenMolokai.sleep();
 				}
 				else if (boatOnOahu && inBoat == 1) {
-					onOahu = false;
 					oChild--;
+					onOahu = false;
 					bg.ChildRideToMolokai();
 					boatOnOahu = false;
 					inBoat = 0;
@@ -133,13 +123,13 @@ public class Boat {
 						done.V();
 						childrenMolokai.sleep();
 					}
-					childrenMolokai.wakeAll();
-					childrenMolokai.sleep();
+					//childrenMolokai.wakeAll();
+					//childrenMolokai.sleep();
 				}
 				else
 					childrenOahu.sleep();
 			}
-			else {
+			else {//if Molokai
 				if (!boatOnOahu) {
 					bg.ChildRowToOahu();
 					oChild++;
@@ -159,32 +149,10 @@ public class Boat {
 		// all of them on the boat). Please also note that you may not
 		// have a single thread calculate a solution and then just play
 		// it back at the autograder -- you will be caught.
-		System.out
-				.println("\n ***Everyone piles on the boat and goes to Molokai***");
-		bg.AdultRowToMolokai();
-		bg.ChildRideToMolokai();
-		bg.AdultRideToMolokai();
-		bg.ChildRideToMolokai();
-	}
-
-	static int oAdult, oChild;
-	static boolean boatOnOahu;
-	static int inBoat;
-	static Lock commonLock;
-	static Condition2 adultsOahu, adultsMolokai;
-	static Condition2 childrenOahu, childrenMolokai;
-	static Semaphore done;
-}
-/*static void SampleItinerary()
-	{
-		// Please note that this isn't a valid solution (you can't fit
-		// all of them on the boat). Please also note that you may not
-		// have a single thread calculate a solution and then just play
-		// it back at the autograder -- you will be caught.
 		System.out.println("\n ***Everyone piles on the boat and goes to Molokai***");
 		bg.AdultRowToMolokai();
 		bg.ChildRideToMolokai();
 		bg.AdultRideToMolokai();
 		bg.ChildRideToMolokai();
 	}
-}*/
+}
